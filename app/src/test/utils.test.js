40c52bdb3e1fbc67724b1d904f023086a36b4123
johnnyconroy <jsknap@gmail.com>
2018-03-06 21:50:55 +0100

    add front-end tests

---
+++ app/src/test/utils.test.js
@@ -0,0 +1,54 @@
+import encrypt from '../utils/encrypt';
+import parseDates from '../utils/parseDates';
+import sampleDates from '../constants/sampleDates';
+import sampleDatesStringify from '../constants/sampleDatesStringify';
+import { customFetch, customPost, prefixURL } from '../utils/fetchHelpers';
+
+it('encrypt', () => {
+  const password = 'my password';
+  const email = 'my email';
+  const hash = encrypt(password, email);
+  expect(hash).toBe('f79b950fb602fa37c77177140564e6126421f79438e6f9205d44bf0a088675ac');
+});
+
+it('parseDates', () => {
+  const parsed = parseDates(sampleDatesStringify);
+  expect(parsed).toEqual(sampleDates);
+});
+
+it('customFetch', () => {
+  const url = 'https://api.ipify.org/?format=json';
+  const ip = { ip: '176.185.194.90' };
+  return customFetch(url).then(data => (
+    expect(data).toEqual(ip)
+  ));
+});
+
+const prefix = Math.random().toString(36).substring(7);
+const email = `${prefix}@gmail.com`;
+// only run when API is running
+it('customPost - signup', () => {
+  const url = prefixURL('api/auth/signup');
+  const postData = {
+    username: 'TheSnake',
+    email,
+    password: 'Thesnake6',
+    confirmPassword: 'Thesnake6',
+  };
+  return customPost(url, postData).then(data => (
+    expect(data).toEqual({})
+  ));
+});
+
+// only run when API is running
+it('customPost - login', () => {
+  const url = prefixURL('api/auth/login');
+  const password = encrypt('Thesnake6', email);
+  const postData = {
+    email,
+    password,
+  };
+  return customPost(url, postData).then(data => (
+    expect(data).toEqual({})
+  ));
+});
